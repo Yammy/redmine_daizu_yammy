031030171628d61b93857b75aa140b32be75dda5
daipresents <daipresents@56a5d8ea-923b-0410-96d0-81052320384d>
2009-02-08 04:28:54 +0000

    チケットレポート表示機能を実装 とりあえず完成。

--- app/controllers/daizu_ticket_report_controller.rb
+++ app/controllers/daizu_ticket_report_controller.rb
@@ -1,24 +1,33 @@
 class DaizuTicketReportController < ApplicationController
-  before_filter :get_all_user
+  before_filter :get_param, :get_all_user, :get_target_usr
   
   def index
-    assigned_to_id = params[:assigned_to_id]
-    start_date = params[:start_date]
-    due_date = params[:due_date]
           
-    if assigned_to_id && start_date && due_date
+    if @assigned_to_id && @start_date && @due_date
       # get issues
-      projects = Project.find :all,
-                        :conditions => Project.visible_by(User.current)
-
-      @all_issues = Hash.new("0")
-      projects.each do |project|
-        @all_issues[project.name] = project.issues
-      end
+      
+      # get started issue.
+      @all_issues =
+        Issue.find(:all,
+          :conditions => ["start_date >= ? and due_date <= ? and assigned_to_id = ?",
+            @start_date, @due_date, @assigned_to_id],
+          :order => "start_date")
     end
   end
 
+  def get_param
+    @assigned_to_id = params[:assigned_to_id]
+    @start_date = params[:start_date]
+    @due_date = params[:due_date]
+  end
+  
   def get_all_user
-    @users =  User.find :all
+    @all_users =  User.find(:all, :conditions => ["status = 1"])
+  end
+
+  def get_target_usr
+    if @assigned_to_id
+      @target_user = User.find(:first, :conditions => ["id = ?", @assigned_to_id])
+    end
   end
 end
