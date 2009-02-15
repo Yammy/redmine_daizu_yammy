f70b8eb202a620d16a8ca21121d48c55e1e38df8
daipresents <daipresents@56a5d8ea-923b-0410-96d0-81052320384d>
2009-02-15 07:48:33 +0000

    バグレポートを追加

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_daizu do
   name 'Redmine Daizu plugin'
   author 'Dai Fujihara'
   description 'This is a Reporting and Statistics plugin for Redmine'
-  version '0.3.0'
+  version '0.4.0'
 
   menu :application_menu, :redmine_daizu, { :controller => 'daizu_main', :action => 'index' },
   :caption => :plugin_name, :last => true
