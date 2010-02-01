366f520371434c6b7a3f824bdf902b4e1542220e
YAMASAKI Yutaka <y.yutaka@gmail.com>
2010-02-01 15:23:44 +0900

    担当者のselectedを処理追加 終了したチケットの表示/非表示を追加

--- app/controllers/daizu_ticket_report_controller.rb
+++ app/controllers/daizu_ticket_report_controller.rb
@@ -5,13 +5,13 @@ class DaizuTicketReportController < ApplicationController
           
     if @assigned_to_id && @start_date && @due_date
       # get started issue.
-      #if @disp_end == 1
-        #@all_issues =
-          #Issue.find_by_sql(["SELECT *, (SELECT value FROM custom_values WHERE custom_field_id = 3 AND customized_id = issues.id) AS real_start_date, (SELECT value FROM custom_values WHERE custom_field_id = 4 AND customized_id = issues.id) AS real_due_date, (SELECT SUM(hours) FROM time_entries WHERE issue_id = issues.id) AS work_hour FROM issues WHERE start_date >= :start_date AND due_date <= :due_date AND assigned_to_id = :assigned_to_id ORDER BY start_date ASC", {:start_date => @start_date, :due_date => @due_date, :assigned_to_id => @assigned_to_id}])
-      #eles
+      if @disp_end
+        @all_issues =
+          Issue.find_by_sql(["SELECT *, (SELECT value FROM custom_values WHERE custom_field_id = 3 AND customized_id = issues.id) AS real_start_date, (SELECT value FROM custom_values WHERE custom_field_id = 4 AND customized_id = issues.id) AS real_due_date, (SELECT SUM(hours) FROM time_entries WHERE issue_id = issues.id) AS work_hour FROM issues WHERE start_date >= :start_date AND due_date <= :due_date AND assigned_to_id = :assigned_to_id ORDER BY start_date ASC", {:start_date => @start_date, :due_date => @due_date, :assigned_to_id => @assigned_to_id}])
+      else
         @all_issues =
           Issue.find_by_sql(["SELECT *, (SELECT value FROM custom_values WHERE custom_field_id = 3 AND customized_id = issues.id) AS real_start_date, (SELECT value FROM custom_values WHERE custom_field_id = 4 AND customized_id = issues.id) AS real_due_date, (SELECT SUM(hours) FROM time_entries WHERE issue_id = issues.id) AS work_hour FROM issues WHERE start_date >= :start_date AND due_date <= :due_date AND assigned_to_id = :assigned_to_id AND status_id NOT IN (SELECT id FROM issue_statuses WHERE is_closed = 1) ORDER BY start_date ASC", {:start_date => @start_date, :due_date => @due_date, :assigned_to_id => @assigned_to_id}])
-      #end
+      end
     end
   end
 
